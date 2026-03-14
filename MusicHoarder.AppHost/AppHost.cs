36dde86fcdbcd0a86960ca7da9f2be8eec43969f
Jeffreyyvdb <60582071+Jeffreyyvdb@users.noreply.github.com>
2026-03-14 16:09:17 +0100

    Add DataVolume

--- MusicHoarder.AppHost/AppHost.cs
+++ MusicHoarder.AppHost/AppHost.cs
@@ -1,6 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume("musichoarder-volume");
 var postgresdb = postgres.AddDatabase("musichoarderdb");
 
 builder.AddProject<Projects.MusicHoarder_Api>("musichoarder-api")
